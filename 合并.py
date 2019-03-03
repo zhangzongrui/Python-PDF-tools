a6bcdf192cffbd45b40fb1d933fa5e7fddc18b3a
Stealers <44942680+Stealers@users.noreply.github.com>
2019-03-03 22:28:55 +0800

    Update 合并.py

--- 合并.py
+++ 合并.py
@@ -1,14 +1,22 @@
 import fitz
 
+#合并
 def merge(doc1,doc2):
-    doc=doc1[0:-4]
+    Doc=doc1[0:-4]
     doc1=fitz.open(doc1)
     doc2=fitz.open(doc2)
     doc3 = fitz.open()
     doc3.insertPDF(doc1)
     doc3.insertPDF(doc2)
-    doc3.save(doc+"(合并).pdf")
+    doc3.save(Doc+"(合并).pdf")
+    #print("操作完成，文件以保存在:\n"+Doc+"(合并).pdf")
+    m1="操作完成，文件以保存在:\n"+Doc+"(合并).pdf"
+    return m1
 
+def main():
 doc1="F:/Python/test1.pdf"
 doc2="F:/Python/test2.pdf"
 merge(doc1,doc2)
+
+if __name__ == '__main__':
+    main()
