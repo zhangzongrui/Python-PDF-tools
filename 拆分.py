f9f615f232283fdd59fe6a4d845a87f48321e324
Stealers <44942680+Stealers@users.noreply.github.com>
2019-03-03 22:29:49 +0800

    Update 拆分.py

--- 拆分.py
+++ 拆分.py
@@ -1,12 +1,20 @@
 import fitz
 
-def split(doc):
+#拆分
+def split(doc, from_pa, to_pa):
     Doc=doc[0:-4]
     doc=fitz.open(doc)
     DOC = fitz.open()
-    a=1;b=3
-    DOC.insertPDF(doc,from_page=a-1,to_page=b-1)
+    DOC.insertPDF(doc,from_page=from_pa-1,to_page=to_pa-1)
     DOC.save(Doc+"(拆分).pdf")
+    #print("操作完成，文件以保存在:"+Doc+"(拆分).pdf")
+    s1="\n操作完成，文件以保存在:\n"+Doc+"(拆分).pdf"
+    return s1
 
-doc="F:/Python/test.pdf"
-split(doc)
+def main():
+    doc="F:/Python/test.pdf"
+    split(doc)
+
+if __name__ == '__main__':
+    main()
+
