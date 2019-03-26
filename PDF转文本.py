405f825de4e13e24d43be35bdb8ef696b8b00ce6
Stealers <44942680+Stealers@users.noreply.github.com>
2019-03-26 22:17:21 +0800

    Update and rename PDF转Word.py to PDF转文本.py

--- PDF转文本.py
+++ PDF转文本.py
@@ -1,9 +1,9 @@
 def pdfToword(doc):
     Doc=doc[0:-4]
     doc = fitz.open(doc)                  
-    DOC = open(Doc + ".doc","w")
+    DOC = open(Doc + ".doc","wb")
     for page in doc:                            
-        text = page.getText()   
+        text = page.getText().encode("utf8")   
         DOC.write(text)
     DOC.close()
     #print("操作完成，文件以保存在:"+Doc+".doc")
